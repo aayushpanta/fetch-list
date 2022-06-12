2adddb1ee6f5a92838bbc7f70545a7afabf2dbfd
Aayush <aayushpanta2612@gmail.com>
2022-06-12 13:22:46 +0545

    Sliding  About

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -4,7 +4,9 @@ const About = () => {
   return (
     <div className='container'>
         <h3>About</h3>
-      This is a simple React app that uses fetch api. Data is fetched from <a href='https://jsonplaceholder.ir/users'>"jsonplaceholder.ir/users"</a>.
+        <div className='w3-animate-left'>
+             This is a simple React app that uses fetch api. Data is fetched from <a href='https://jsonplaceholder.ir/users'>"jsonplaceholder.ir/users"</a>.
+        </div>
     </div>
   )
 }
